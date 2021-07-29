0387cd6fd894a295337981e1c5d72d566f4ca0f8
Etienne BENDER <bender.etienne57@gmail.com>
2021-07-30 00:27:31 +0200

    Use a const to simulate users list

--- src/index.js
+++ src/index.js
@@ -2,17 +2,20 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const users = [];
+
 app.get('/', (req, res) => {
 	res.send('Hello World!');
 });
 app.get('/users', ((req, res) => {
-	res.send([]);
+	res.send(users);
 }));
 app.get('/user/:id', ((req, res) => {
 	res.send({id:0});
 }));
 app.post('/user', ((req, res) => {
-	res.send({ id: 0 });
+	users.push({id: users.length});
+	res.send(users[users.length-1]);
 }));
 
 app.listen(port);
